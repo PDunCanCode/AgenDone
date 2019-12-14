a43eca1be7e7b73465750cf6ccac72081e2cb0dd
Paul D. Duncan <chesiredarling@gmail.com>
2019-12-13 21:50:58 -0800

    FirebaseConfig plus plus

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = firebase.initiallizapp({
+    apiKey: "AIzaSyA0G4j_s1xwQPOf9_W9mr1OT6HAxW0olZs",
+    authDomain: "agendone-664b0.firebaseapp.com",
+    databaseURL: "https://agendone-664b0.firebaseio.com",
+    projectId: "agendone-664b0",
+    storageBucket: "agendone-664b0.appspot.com",
+    messagingSenderId: "400399745461",
+    appId: "1:400399745461:web:d8c090672858520b8859fc"
+});
+
+export { firebaseConfig as firebase };
